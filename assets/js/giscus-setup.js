a55cd8bde883b487cbb0487055e4a629445b099c
anirbanchandra1 <anirbanchandra1@gmail.com>
2026-03-14 02:18:01 +0000

    Deploying to gh-pages from @ anirbanchandra1/anirbanchandra1.github.io@9cfdc6a4484ded1ca5d2a3a81688d133d771c913 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "anirbanchandra1/test-portfolio",
+    "data-repo": "anirbanchandra1/anirbanchandra1.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
